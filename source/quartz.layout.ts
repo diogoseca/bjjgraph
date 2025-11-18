d192aa890ce0c387de4185ba3672b20f123eb0d7
Diogo Seca <diogoseca@gmail.com>
2025-11-18 03:40:05 +0000

    Add PostHog Feedback

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -5,7 +5,8 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [Component.DevelopmentBanner()],
+  //afterBody: [Component.DevelopmentBanner()],
+  afterBody: [],
   // Footer with no links - may add social links later
   footer: Component.Footer({
     links: {},
